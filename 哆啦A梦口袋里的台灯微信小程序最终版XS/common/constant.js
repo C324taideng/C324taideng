0822c3900f2dfab0a3f787aa13bc28a0d5a07f54
zzh3433 <45479443+zzh3433@users.noreply.github.com>
2018-12-07 23:25:24 +0800

    Add files via upload

---
+++ 哆啦A梦口袋里的台灯微信小程序最终版XS/common/constant.js
@@ -0,0 +1,6 @@
+const API_KEY="9dc7ab2f8993b0b215ad8c550e1f4ebe";
+const WEATHER_URL = "http://wthrcdn.etouch.cn/weather_mini";
+module.exports = {
+    WEATHER_URL: WEATHER_URL,
+    API_KEY:API_KEY
+}
---
+++ 哆啦A梦口袋里的台灯微信小程序最终版XS/config.js
@@ -0,0 +1,14 @@
+/**
+ * 小程序配置文件
+ */
+
+module.exports = {
+
+  //NLI appkey
+  appkey: `1b102859e7a24febba917b7b4d8421c6`,
+
+  //NLI appsecret
+  appsecret: `211d976af7eb4a53aed23439413c6376`,
+
+ 
+};
